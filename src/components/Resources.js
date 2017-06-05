30f7b47c0bf4b6c16716bf59ac06d2f6f972bed5
Mick Johnson <mickajohnson@outlook.com>
2017-06-04 20:11:27 -0500

    first commit

---
+++ src/components/Resources.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Super Secret Stuff
+      <ul>
+        <li>Not</li>
+        <li>For</li>
+        <li>You</li>
+        <li>!!!</li>
+      </ul>
+    </div>
+  )
+}
